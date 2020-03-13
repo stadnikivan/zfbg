3a6bde30da0748b66ad8fbee15086ab6731c0616
stadnikivan <stadnik11ivan@gmail.com>
2020-03-13 09:31:27 +0200

    Updates

---
+++ precache-manifest.f63ad39d1bc91a3ec49ea97a06b0733e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b755033c4e6f509b3dba52d17ec42f27",
+    "url": "/zfbg/index.html"
+  },
+  {
+    "revision": "8fd566ecea2acdeb35cf",
+    "url": "/zfbg/static/css/main.eacd5da5.chunk.css"
+  },
+  {
+    "revision": "10481d85c56a6e35079b",
+    "url": "/zfbg/static/js/2.5232c8bf.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/zfbg/static/js/2.5232c8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd566ecea2acdeb35cf",
+    "url": "/zfbg/static/js/main.39168783.chunk.js"
+  },
+  {
+    "revision": "e1293805fd8b86b1547e",
+    "url": "/zfbg/static/js/runtime-main.340fdda4.js"
+  }
+]);
